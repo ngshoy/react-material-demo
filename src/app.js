4d75f99d26c957b97a1340c74f7a779744071d60
Jack Zhang <ngshoy@hotmail.com>
2019-07-28 14:35:02 -0400

    added material ui

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {
-  render
-} from 'react-dom';
+import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
 import App from './components';
+import theme from './theme';
 
-render( 
-  <BrowserRouter>
-    <App / > 
-  </BrowserRouter>,
+render(
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <CssBaseline />
+      <App / > 
+    </BrowserRouter>
+  </ThemeProvider> ,
   document.getElementById('app-root')
 )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
 
 const Header = () => (
   <div>
-    <NavLink to='/' >Home</NavLink>
-    <NavLink to='/About'>About</NavLink>
+    <NavLink to='/'><Button color='primary'>Home</Button></NavLink>
+    <NavLink to='/About'><Button color='primary'>About</Button></NavLink>
   </div>
 );
 
